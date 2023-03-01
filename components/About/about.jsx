cf1b3a69de96195c8260670555646cb46e71f6fb
AdrianBuican <124651077+AdrianBuican@users.noreply.github.com>
2023-03-01 16:51:57 +0200

    Update about.jsx

--- components/About/about.jsx
+++ components/About/about.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import './About.css'
+import './about.css'
 import image1 from '../../assets/image1.jpg'
 import image2 from '../../assets/image2.jpg'
 import image3 from '../../assets/image3.jpg'
