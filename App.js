24be7e41fd57cf315347680981a0af119523db9f
AdrianBuican <124651077+AdrianBuican@users.noreply.github.com>
2023-03-01 16:33:31 +0200

    Update App.js

--- App.js
+++ App.js
@@ -1,9 +1,18 @@
 import './App.css';
-import Hero from './components/Hero/Hero'
+import Home from './components/Home/Home'
+import Programs from './components/programs/programs'
+import About from './components/About/About.jsx'
+import Plans from './components/Plans/plans.jsx'
+import Testimonials from './components/Testimonials/testimonials.jsx'
+
 function App() {
   return (
     <div className="App">
-          <Hero />
+          <Home />
+          <Programs />
+          <About />
+          <Plans />
+          <Testimonials />
     </div>
   );
 }
