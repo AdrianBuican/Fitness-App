c49b3c601695a95e53a2ac1476bbd0ffeee3bda5
AdrianBuican <124651077+AdrianBuican@users.noreply.github.com>
2023-03-03 16:25:00 +0200

    Update App.js

--- App.js
+++ App.js
@@ -1,18 +1,22 @@
 import './App.css';
-import Home from './components/Home/Home'
-import Programs from './components/programs/programs'
-import About from './components/About/About.jsx'
+import Home from './components/Home/home'
+import Workouts from './components/Workouts/workouts.jsx'
+import About from './components/About/about.jsx'
 import Plans from './components/Plans/plans.jsx'
 import Testimonials from './components/Testimonials/testimonials.jsx'
+import Join from './components/Join/join.jsx'
+import Footer from './components/Footer/footer.jsx'
 
 function App() {
   return (
     <div className="App">
           <Home />
-          <Programs />
+          <Workouts />
           <About />
           <Plans />
           <Testimonials />
+          <Join />
+          <Footer />
     </div>
   );
 }
