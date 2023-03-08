04616953432c96a36188c0c336eed2bbf7768d69
AdrianBuican <124651077+AdrianBuican@users.noreply.github.com>
2023-03-08 11:43:02 +0200

    Add files via upload

---
+++ components/Blurs/blurs.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import './blurs.css'
+
+const Blurs = () => {
+   return(
+      <div>
+         <div className="blur-1"></div>
+         <div className="blur-2"></div>
+         <div className="blur-3"></div>
+      </div>
+   )
+}
+
+export default Blurs
