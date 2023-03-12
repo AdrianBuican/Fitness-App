247056f236689ae6a37096ad661d468ff48f9a1a
AdrianBuican <124651077+AdrianBuican@users.noreply.github.com>
2023-03-12 10:35:08 +0200

    Update header.jsx

--- components/Header/header.jsx
+++ components/Header/header.jsx
@@ -1,19 +1,21 @@
 import React from 'react';
 import './header.css';
-import Logo from '../../assets/logo.png'
+import Logo from '../../assets/logo.jpg'
 
 const Header = () => {
    return(
+      <>
       <div className="header">
-         <img src={Logo} className="logo"/>
+         <img src={Logo} className="logo" alt="Logo"/>
          <ul className="header-menu">
             <li>Home</li>
             <li>Workouts</li>
-            <li>Why us</li>
+            <li>About Us</li>
             <li>Plans</li>
             <li>Testimonials</li>
          </ul>
       </div>
+      </>
    )
 }
 
