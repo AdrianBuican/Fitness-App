cc48ad7ef6f8abfc59da0b002f8675965c07c84d
AdrianBuican <124651077+AdrianBuican@users.noreply.github.com>
2023-03-21 03:29:26 +0200

    Add files via upload

---
+++ data/testimonialsData.js
@@ -0,0 +1,39 @@
+import image1 from "../assets/t-image1.jpg";
+import image2 from "../assets/t-image2.jpg";
+import image3 from "../assets/t-image3.jpg";
+import image4 from "../assets/t-image4.jpg";
+import image5 from "../assets/t-image5.jpg";
+
+export const testimonialsData = [
+  {
+    image: image1,
+    review: '"iSculpt is legit the only app that made me fall in love with training. Totally recommend it!"',
+    name: "Evelyn Moore",
+    status: 'Customer'
+  },
+  {
+    image: image2,
+    review:
+      '"Using the iSculpt app was a great choice and I made considerable progress with their programs!"',
+    name: 'Michael Ahmad',
+    status : 'Customer'
+  },
+  {
+    image: image3,
+    review: '"iSculpt taught me how easy it is to stay in shape if you have a smart plan."',
+    name: "Alissa Rodriguez",
+    status: 'Customer'
+  },
+  {
+    image : image4,
+    review: '"I always believed I was a hard gainer, but the program provided by the iSculpt coaches worked like magic!"',
+    name: 'Gabriel Jackson',
+    status: "Customer"
+  },
+  {
+    image : image5,
+    review: '"Looking back, using iSculpt is the best decision I made in the last year, at least regarding fitness"',
+    name: "Kevin O'Connor",
+    status: 'Customer'
+  },
+];
