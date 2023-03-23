dd0c7a8b2b6b766796bc94f492bf95f380455c3d
AdrianBuican <124651077+AdrianBuican@users.noreply.github.com>
2023-03-23 23:09:13 +0200

    Update testimonials.jsx

--- components/Testimonials/testimonials.jsx
+++ components/Testimonials/testimonials.jsx
@@ -4,6 +4,8 @@ import './testimonials.css'
 import {testimonialsData} from '../../data/testimonialsData.js'
 import rightArrow from '../../assets/rightArrow.png'
 import leftArrow from '../../assets/leftArrow.png'
+import testimonialAnimation from './testimonialAnimation.js'
+import LetterSwap2 from './letterSwapTestimonials.js'
 
 const Testimonials = () => {
    const [selected, setSelected] = useState(0);
@@ -15,12 +17,12 @@ const Testimonials = () => {
       <div className="testim">
          <div className="testim-left">
             <span>WHAT PEOPLE SAY ABOUT </span>
-            <span className="rose-text">i<span className="orange-text">Sculpt</span></span>
-            <span className="customer-reviews">
+            <span className="persimmon-text">i<span className="orange-text" id="sculpt2" onMouseOver={LetterSwap2}>Sculpt</span></span>
+            <span className="customer-reviews" id="review">
                {testimonialsData[selected].review}
             </span>
             <span className="customer-reviews">
-               <span>
+               <span className="testim-name">
                   {testimonialsData[selected].name + " "} 
                </span>
                - {testimonialsData[selected].status}
@@ -30,23 +32,25 @@ const Testimonials = () => {
          <div className="testim-right">
             <div></div>
             <div></div>
-            <img src={testimonialsData[selected].image} alt="" />
+            <img src={testimonialsData[selected].image} alt="image" className="testim-image"/>
             <div className="arrows">
                <img 
-               onClick={() =>{
-               selected===0
-               ? setSelected(tLength -1)
-               : setSelected((prev) => prev-1)
-               }}
-               src={leftArrow} alt="" />
+               onClick= {() =>{
+                  selected===0
+                  ? setSelected(tLength -1)
+                  : setSelected((prev) => prev-1);
+                  testimonialAnimation()
+                  }}
+               src={leftArrow} alt="Left Arrow" className="left-arrow"/>
 
                <img 
                onClick = {() => {
                   selected === tLength -1
                   ? setSelected(0)
-                  : setSelected((prev) => prev+1)
+                  : setSelected((prev) => prev+1);
+                  testimonialAnimation()
                }}
-               src={rightArrow} alt="" />
+               src={rightArrow} alt="Right Arrow" className="right-arrow" />
             </div>
          </div>
       </div>
